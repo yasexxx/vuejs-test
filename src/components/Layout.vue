1bf981e86b5acfe32b0566107bf4f92fa8ba38d6
yasexxx <hildebrandlig11@gmail.com>
2022-09-28 17:55:31 +0800

    adding vue code

---
+++ src/components/Layout.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Header from './Header.vue';
+
+    export default defineComponent({
+    methods: {},
+    components: { Header }
+})
+</script>
+
+<template>
+    <Header />
+    <slot></slot>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import Layout from "../components/Layout.vue";
+
+</script>
+
+<template>
+  <div class=" homeContainer">
+    <div class="px-4 container mx-auto">
+      <Layout>
+        <div>Hello</div>
+      </Layout>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.homeContainer {
+  height: 100vh;
+  background-color: #151515;
+}
+
+@media screen and (min-width: 960px) {
+}
+</style>
